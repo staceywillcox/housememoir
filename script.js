4a8e9e1d59c18b0cb4bbc4aef375be5d0271a266
Guest User <Guest@wg401-i-03-07.mddn.vuw.ac.nz>
2018-05-07 16:09:28 +1200

    added navigation

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-document.getElementbyId('')
+
+
+
+btnPast.addEventListener("click", e =>{
+ document.getElementById('present').classList.add('hide');
+ document.getElementById('future').classList.add('hide');
+ document.getElementById('past').classList.remove('hide');
+});
+
+btnPresent.addEventListener("click", e =>{
+ document.getElementById('present').classList.remove('hide');
+ document.getElementById('future').classList.add('hide');
+ document.getElementById('past').classList.add('hide');
+});
+
+btnFuture.addEventListener("click", e =>{
+ document.getElementById('present').classList.add('hide');
+ document.getElementById('future').classList.remove('hide');
+ document.getElementById('past').classList.add('hide');
+});
+
